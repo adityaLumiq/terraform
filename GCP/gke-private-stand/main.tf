46a6e77d22c0cc94c0ed4ced61b7a83c9f29b759
Gautam <jigyasu.gautam@lumiq.ai>
2023-07-13 17:36:12 +0530

    added GKE files

--- GCP/gke-private-stand/main.tf
+++ GCP/gke-private-stand/main.tf
@@ -1,33 +1,171 @@
-resource "google_container_cluster" "private_cluster" {
-  name               = var.cluster_name
-  location           = var.region
-  remove_default_node_pool = true
+# create VPC
+resource "google_compute_network" "vpc" {
+  name                    = var.network
+  auto_create_subnetworks = false
+}
 
-  network            = google_compute_network.private_network.self_link
-  subnetwork         = google_compute_subnetwork.private_subnetwork.self_link
+# Create Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = var.subnetwok
+  region        = var.region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = var.subnet_range
+}
 
+# Create Service Account
+ resource "google_service_account" "mysa" {
+   account_id   = var.service_account
+   display_name = "Service Account for GKE nodes"
+}
+
+resource "google_project_iam_member" "firestore_owner_binding" {
+  project = var.project_id
+  role    = "roles/datastore.owner"
+  member  = "serviceAccount:${var.serv}"
+}
+
+# Create GKE cluster with 2 nodes in our custom VPC/Subnet
+resource "google_container_cluster" "primary" {
+  name                     = var.cluster_name
+  zone                     = var.zone
+  network                  = google_compute_network.vpc.name
+  subnetwork               = google_compute_subnetwork.subnet.name
+  remove_default_node_pool = true                ## create the smallest possible default node pool and immediately delete it.
+  networking_mode          = "VPC_NATIVE" 
+  initial_node_count       = 1
+  
   private_cluster_config {
-    enable_private_endpoint  = true
-    enable_private_nodes     = true
-    master_ipv4_cidr_block   = var.cluster_ipv4_cidr
+    enable_private_endpoint = true
+    enable_private_nodes   = true 
+    master_ipv4_cidr_block = "10.13.0.0/28"
   }
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block  = "10.11.0.0/21"
+    services_ipv4_cidr_block = "10.12.0.0/21"
+  }
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block   = "10.0.0.7/32"
+      display_name = "net1"
+    }
 
-  master_auth {
-    username = ""
-    password = ""
+  }
+}
 
-    client_certificate_config {
-      issue_client_certificate = false
+# Create managed node pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = google_container_cluster.primary.name
+  location   = "asia-south2-a"
+  cluster    = google_container_cluster.primary.name
+  node_count = 3
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = "dev"
+    }
+
+    machine_type = "n1-standard-1"
+    preemptible  = true
+    #service_account = google_service_account.mysa.email
+
+    metadata = {
+      disable-legacy-endpoints = "true"
     }
   }
 }
 
-data "google_compute_network" "private_network" {
-  name = var.network
+
+
+## Create jump host . We will allow this jump host to access GKE cluster. the ip of this jump host is already authorized to allowin the GKE cluster
+
+resource "google_compute_address" "my_internal_ip_addr" {
+  project      = "tcb-project-371706"
+  address_type = "INTERNAL"
+  region       = "asia-south2"
+  subnetwork   = "subnet1"
+  name         = "my-ip"
+  address      = "10.0.0.7"
+  description  = "An internal IP address for my jump host"
 }
 
-data "google_compute_subnetwork" "private_subnetwork" {
-  name          = var.subnetwork
-  network       = data.google_compute_network.private_network.self_link
-  region        = var.region
+resource "google_compute_instance" "default" {
+  project      = "tcb-project-371706"
+  zone         = "asia-south2-a"
+  name         = "jump-host"
+  machine_type = "e2-medium"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network    = "vpc1"
+    subnetwork = "subnet1" # Replace with a reference or self link to your subnet, in quotes
+    network_ip         = google_compute_address.my_internal_ip_addr.address
+  }
+
+}
+
+
+## Creare Firewall to access jump hist via iap
+
+
+resource "google_compute_firewall" "rules" {
+  project = "tcb-project-371706"
+  name    = "allow-ssh"
+  network = "vpc1" # Replace with a reference or self link to your network, in quotes
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["35.235.240.0/20"]
+}
+
+
+
+## Create IAP SSH permissions for your test instance
+
+resource "google_project_iam_member" "project" {
+  project = "tcb-project-371706"
+  role    = "roles/iap.tunnelResourceAccessor"
+  member  = "serviceAccount:terraform-demo-aft@tcb-project-371706.iam.gserviceaccount.com"
+}
+
+# create cloud router for nat gateway
+resource "google_compute_router" "router" {
+  project = "tcb-project-371706"
+  name    = "nat-router"
+  network = "vpc1"
+  region  = "asia-south2"
+}
+
+## Create Nat Gateway with module
+
+module "cloud-nat" {
+  source     = "terraform-google-modules/cloud-nat/google"
+  version    = "~> 1.2"
+  project_id = "tcb-project-371706"
+  region     = "asia-south2"
+  router     = google_compute_router.router.name
+  name       = "nat-config"
+
+}
+
+
+############Output############################################
+output "kubernetes_cluster_host" {
+  value       = google_container_cluster.primary.endpoint
+  description = "GKE Cluster Host"
+}
+
+output "kubernetes_cluster_name" {
+  value       = google_container_cluster.primary.name
+  description = "GKE Cluster Name"
 }
