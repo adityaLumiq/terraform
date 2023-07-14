5aed4431fac4510129d8235ecb788caec4b62632
Gautam <jigyasu.gautam@lumiq.ai>
2023-07-14 17:34:09 +0530

    added sa

--- GCP/service-account/main.tf
+++ GCP/service-account/main.tf
@@ -1,25 +1,44 @@
-# Create Service Account
-resource "google_service_account" "service_account" {
-  project      = var.project
-  account_id   = var.name
-  display_name = var.description
+resource "google_service_account" "composer_sa" {
+  account_id   = var.composer_sa
+  display_name = "Composer Service Account"
 }
-
-# ADD ROLES TO SERVICE ACCOUNT
-
-locals {
-  all_service_account_roles = concat(var.service_account_roles, [
-    "roles/logging.logWriter",
-    "roles/monitoring.metricWriter",
-    "roles/monitoring.viewer",
-    "roles/stackdriver.resourceMetadata.writer"
+resource "google_project_iam_member" "composer_sa_role" {
+  for_each  = toset([
+    "roles/bigquery.dataEditor",
+    "roles/bigquery.resourceEditor",
+    "roles/bigquery.user",
+    "roles/composer.admin",
+    "roles/composer.sharedVpcAgent",
+    "roles/composer.user",
+    "roles/composer.worker",
+    "roles/dataflow.developer",
+    "roles/storage.objectViewer",
+    "roles/container.developer",
+    "roles/secretmanager.secretAccessor",
+    "roles/iam.serviceAccountUser",
+    "roles/storage.objectCreator",
+    "roles/storage.objectViewer",
   ])
+  role = each.value
+  member = "serviceAccount:${google_service_account.composer_sa.email}"
 }
 
-resource "google_project_iam_member" "service_account-roles" {
-  for_each = toset(local.all_service_account_roles)
-
-  project = var.project
-  role    = each.value
-  member  = "serviceAccount:${google_service_account.service_account.email}"
+resource "google_service_account" "other_sa" {
+  account_id   = var.other_sa
+  display_name = "Other Service Account"
+}
+resource "google_project_iam_member" "composer_sa_role" {
+  for_each  = toset([
+    "roles/bigquery.dataEditor",
+    "roles/bigquery.filteredDataViewer",
+    "roles/cloudkms.cryptoKeyEncrypterDecrypter",
+    "roles/dataflow.developer",
+    "roles/dataflow.viewer",
+    "roles/dataflow.worker",
+    "roles/secretmanager.secretAccessor",
+    "roles/storage.objectCreator",
+    "roles/storage.objectViewer",
+  ])
+  role = each.value
+  member = "serviceAccount:${google_service_account.other.email}"
 }
--- GCP/service-account/variable.tf
+++ GCP/service-account/variable.tf
@@ -1,22 +1,12 @@
-
-variable "project" {
-  description = "The name of the GCP Project where all resources will be launched."
-  type        = string
+variable "composer_sa" {
+  type = string
+  default = "Display Name of Composer Service Account"
+  
 }
 
-variable "name" {
-  description = "The name of the custom service account. This parameter is limited to a maximum of 28 characters."
-  type        = string
+variable "other_sa" {
+  type = string
+  default = "Display name for other SA"
+  
 }
 
-variable "description" {
-  description = "The description of the custom service account."
-  type        = string
-  default     = ""
-}
-
-variable "service_account_roles" {
-  description = "Additional roles to be added to the service account."
-  type        = list(string)
-  default     = []
-}
