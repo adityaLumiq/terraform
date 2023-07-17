a0fef818ab05560aecad3e061dd7234f5e0c8886
Gautam <jigyasu.gautam@lumiq.ai>
2023-07-17 19:34:36 +0530

    updated composer

--- GCP/module_dir/main.tf
+++ GCP/module_dir/main.tf
@@ -14,5 +14,8 @@ module "kms" {
 
 module "compose-autopilot" {
     source = "../composer-autopilot"
+    composer_vpc = var.vpc_network_name
+    composer_subnetwork = var.subnetwork_composer
+    composer_service_account = var.composer_sa
   
 }
