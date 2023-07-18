7777a1c2fb08807e4d437c05eee47852acd9f004
Aditya Sharma <aditya.sharma@lumiq.ai>
2023-07-18 16:49:50 +0530

    updated root main.tf

--- GCP/main.tf
+++ GCP/main.tf
@@ -1,16 +1,33 @@
 module "vpc" {
     source = "./vpc"
-      
+    region = var.region
+    zone = var.zone
+    vpc_network_name = var.vpc_network_name
+    subnetwork_composer = var.subnetwork_composer
+    ip_range_composer = var.ip_range_composer
+    subnetwork_other = var.subnetwork_other
+    ip_range_other = var.ip_range_other
+    ip_range_vpc = var.ip_range_vpc
+    composer_pods = var.composer_pods
+    composer_service = var.composer_service
+
 }
 
 module "service_account" {
     source = "./service-account"
-  
+    composer_sa = var.composer_sa
+    other_sa = var.other_sa
+    project_id = var.project_id
 }
 
 module "kms" {
     source = "./kms"
-  
+    keyring_name = var.keyring_name
+    region = var.region
+    key_name = var.kms_key_name
+    rotation_period = var.rotation_period
+    algorithm = var.algorithm
+    project = var.project
 }
 
 module "composer" {
@@ -24,7 +41,7 @@ module "composer" {
     allow_ip_range = var.allow_ip_range
     master_authorized_networks = var.master_authorized_networks
     image_version = var.image_version
-    kms_key_name = var.kms_key_name
+    key_name = var.kms_key_name
     environment_size = var.environment_size
     composer_vpc = var.vpc_network_name
     composer_subnet = var.subnetwork_composer
---
+++ GCP/variable.auto.tfvars
@@ -0,0 +1,54 @@
+composer_name = "terraform_composer"
+region = "asia-south1"
+image_version = "composer-2.3.3-airflow-2.4.3"
+environment_size = "ENVIRONMENT_SIZE_SMALL"
+scheduler = {
+  cpu        = 2
+  memory_gb  = 7.5
+  storage_gb = 5
+  count      = 2
+}
+web_server = {
+  cpu = "2"
+  memory_gb  = 7.5
+  storage_gb = 5
+}
+worker = {
+  cpu        = 2
+  memory_gb  = 7.5
+  storage_gb = 5
+  min_count  = 2
+  max_count  = 6
+}
+master_authorized_networks =  {
+  cidr_block = "10.3.0.0/28"
+  display_name = "composer-cidr"
+} 
+allow_ip_range = "10.54.2.0/24"
+ip_pods =  {
+  cidr_block = "192.168.0.0/16"
+  display_name = "composer-pod-range"
+} 
+ip_svc =  {
+  cidr_block = "192.168.4.0/16"
+  display_name = "composer-svc-range"
+} 
+
+
+#kms
+keyring_name  = "terraform_composer_keyring"
+kms_key_name = "terraform_composer_key"
+rotation_period = "2592000s" 
+algorithm = "GOOGLE_SYMMETRIC_ENCRYPTION" 
+project = "outstanding-pen-389306"
+
+#vpc
+zone = "asia-south1c"
+vpc_network_name = "terraform-composer"
+subnetwork_composer = "composer-subnet"
+ip_range_composer = "10.54.2.0/24"
+subnetwork_other = "app-subnet"
+ip_range_other = "10.54.1.0/24"
+ip_range_vpc = "10.0.0.0/24"
+composer_pods = "172.16.0.0/16"
+composer_service = "172.17.0.0/16"
