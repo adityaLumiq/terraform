4c0cac42001b96138e9b54448b75b16ec8c28f1e
Gautam <jigyasu.gautam@lumiq.ai>
2023-07-18 16:37:55 +0530

    added main.tf

--- GCP/variable.tf
+++ GCP/variable.tf
@@ -211,18 +211,22 @@ variable "keyring_name" {
   description = "Name of the KMS Keyring"
   type = string
 }
+
 variable "algorithm" {
   description = "Algorithm for the KMS key"
   type = string
 }
+
 variable "region" {
   description = "Region for the KMS key"
   type = string
 }
+
 variable "rotation_period" {
   description = "Time in seconds to rotate key"
   type = string
 }
+
 variable "project" {
   description = "Project id"
   type = string
