970afdb4ce1d2898f33bd84281d575c41c28fae7
Gautam <jigyasu.gautam@lumiq.ai>
2023-07-18 15:27:58 +0530

    composer var updated

--- GCP/composer-autopilot/variable.tf
+++ GCP/composer-autopilot/variable.tf
@@ -10,16 +10,75 @@ variable "region" {
   
 }
 
+variable "allow_ip_range" {
+  type = string
+  description = "Subnet to access Airlflow UI"
+  
+}
+
+variable "master_authorized_networks" {
+  type = object({
+    cidr_block   = string
+    display_name = string
+  })
+  
+  description = "List of master authorized networks. If none are provided, disallow external access (except the cluster node IPs, which GKE automatically whitelists)."
+}
+
 variable "image_version" {
     type = string
     description = "Airflow Image to be used"
   
 }
+
+variable "kms_key_name" {
+  type = string
+  description = "KMS Key to be used"
+  
+}
+
 variable "environment_size" {
   type        = string
   description = "Environment size"
   default     = "ENVIRONMENT_SIZE_SMALL"
 }
+
+variable "composer_vpc" {
+  type = string
+  description = "Composer VPC"
+  
+}
+
+variable "composer_subnet" {
+  type = string
+  description = "Composer Subnet"
+  
+}
+
+variable "composer_sa" {
+  type = string
+  description = "Composer Service Account"
+  
+}
+
+variable "ip_pods" {
+  type = object({
+    cidr_block   = string
+    display_name = string
+  })
+  description = "Pod Ranges"
+
+}
+
+variable "ip_svc" {
+  type = object({
+    cidr_block   = string
+    display_name = string
+  })
+  description = "Svc Ranges"
+}
+
+
 variable "scheduler" {
   type = object({
     cpu        = string
@@ -66,35 +125,4 @@ variable "worker" {
     max_count  = 6
   }
   description = "Configuration for resources used by Airflow workers."
-}
-variable "master_authorized_networks" {
-  type = list(object({
-    cidr_block   = string
-    display_name = string
-  }))
-  default     = []
-  description = "List of master authorized networks. If none are provided, disallow external access (except the cluster node IPs, which GKE automatically whitelists)."
-}
-
-variable "allow_ip_range" {
-  type = string
-  description = "Subnet to access Airlflow UI"
-  
-}
-
-variable "ip_pods" {
-  type = list(object({
-    cidr_block   = string
-    display_name = string
-  }))
-  description = "Pod Ranges"
-
-}
-
-variable "ip_svc" {
-  type = list(object({
-    cidr_block   = string
-    display_name = string
-  }))
-  description = "Svc Ranges"
 }
